1a66a3acdc3a8d489395872fa0d0f6915e09f5fe
Rohan <rohanzealous@gmail.com>
2021-09-02 19:43:24 -0700

    finshed

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,29 +2,62 @@ import java.util.Scanner;
 
 public class MyMain {
 
-    // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        // if a is bigger than b x should b positive
+        int x = a - b;
+        int y = b - c; //same thing 4 y
+        int z = a - c; // ^^^^
+
+        if (x * y > 0) { //is b mid? if xy is pos
+            return b;
+        }
+        else if (z * x > 0) { // is c mid? if xz is pos
+            return c;
+        }
+        return a;
     }
+
 
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (Math.abs(a) > Math.abs(b)) {
+            return a;
+        }
+        else
+            return b;
     }
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
     // where "a" and "b" are the inputs to the method
     public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        return Math.sqrt((a*a)+(b*b));
     }
 
     public static void main(String[] args) {
-        // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
-
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        System.out.println("what u want");
+        String method = scan.next();
+        while (!method.contains("median") && !method.contains("magnitude") && !method.contains("pythagoras")) {
+            System.out.println("try again options are median magnitude pythagoras");
+            method = scan.next();
+        }
+        if (method.contains("median")) {
+            System.out.println("input three numbers");
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+            System.out.println(median(a, b, c));
+        }
+        if (method.contains("magnitude")) {
+            System.out.println("input three numbers");
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            System.out.println(magnitude(a, b));
+        }
+        if (method.contains("pythagoras")) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            System.out.println(pythagoras(a, b));
+        }
     }
 }
